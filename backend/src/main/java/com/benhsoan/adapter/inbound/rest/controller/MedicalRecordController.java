58751e61a742cb72e55ac288613810068fad361f
vantuancaptain-t <nguyenvantuan20122005@gmail.com>
2026-07-31 14:49:41 +0700

    edit wrong api

--- backend/src/main/java/com/benhsoan/adapter/inbound/rest/controller/MedicalRecordController.java
+++ backend/src/main/java/com/benhsoan/adapter/inbound/rest/controller/MedicalRecordController.java
@@ -38,7 +38,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/medical-records")
+@RequestMapping("/medical-records")
 @RequiredArgsConstructor
 @Validated
 public class MedicalRecordController {
