075945e49066c836e0aa094483155b0e2b48df0a
vantuancaptain-t <nguyenvantuan20122005@gmail.com>
2026-08-19 10:43:11 +0700

    fix test

--- backend/src/test/java/com/benhsoan/adapter/inbound/rest/controller/ServiceCatalogControllerTest.java
+++ backend/src/test/java/com/benhsoan/adapter/inbound/rest/controller/ServiceCatalogControllerTest.java
@@ -30,6 +30,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
